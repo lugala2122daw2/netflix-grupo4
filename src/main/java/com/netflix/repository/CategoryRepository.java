15d589e496edf2e4042d855e5c4eeef264c76426
andb13 <andreibuhai@protonmail.com>
2022-08-26 12:00:15 +0200

    Delete cat + dir

--- src/main/java/com/netflix/repository/CategoryRepository.java
+++ src/main/java/com/netflix/repository/CategoryRepository.java
@@ -1,12 +1,7 @@
 package com.netflix.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.repository.query.Param;
 import com.netflix.entity.Category;
 
-@RepositoryRestResource(collectionResourceRel = "category", path = "category")
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    List<Category> findCategoryByNameContains(@Param("name") String name);
 }
