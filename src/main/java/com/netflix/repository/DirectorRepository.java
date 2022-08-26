821946ceb5a85335a5de8da36c481bbe5a5a866a
andb13 <andreibuhai@protonmail.com>
2022-08-26 11:44:52 +0200

    Added name filtering

--- src/main/java/com/netflix/repository/DirectorRepository.java
+++ src/main/java/com/netflix/repository/DirectorRepository.java
@@ -1,9 +1,12 @@
 package com.netflix.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
 import com.netflix.entity.Director;
 
+@RepositoryRestResource(collectionResourceRel = "director", path = "director")
 public interface DirectorRepository extends JpaRepository<Director, Integer>{
-    
+    List<Director> findCDirectorByNameContains(@Param("name") String name);
 }
