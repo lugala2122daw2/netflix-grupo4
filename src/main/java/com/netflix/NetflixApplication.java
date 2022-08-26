1f778c10434981e4c11a70a42c036fee4d973f48
oriolElias <109503419+oriolElias@users.noreply.github.com>
2022-08-26 11:48:43 +0200

    search director by name

--- src/main/java/com/netflix/NetflixApplication.java
+++ src/main/java/com/netflix/NetflixApplication.java
@@ -2,6 +2,9 @@ package com.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class NetflixApplication {
@@ -10,4 +13,11 @@ public class NetflixApplication {
 		SpringApplication.run(NetflixApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet)     {
+		ServletRegistrationBean reg = new ServletRegistrationBean(dispatcherServlet);
+		reg.addUrlMappings("/api/*");
+
+		return reg;
+	}
 }
